2f1ed4225c5e8c4dd1310483f857969e5b946ea9
dineshk7708128404 <35687726+dineshk7708128404@users.noreply.github.com>
2018-02-02 16:18:41 +0530

    Create kd.c

---
+++ kd.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include<math.h>
+ 
+long power (inta, intb);
+ 
+int main()
+{
+    int power=3, num=2;
+    long result;
+ 
+    printf("%d", a);
+    scanf("%d", &b);
+    printf("%d", a);
+    scanf("%d", &a);
+    result = power(num, pow);
+    printf("%d^%d is %ld", b, a, result);
+    return 0;
+}
+ 
+long power (int a, int b)
+{
+    if (a)
+    {
+        return (b * a(b ,a- 1));
+    }
+    return 1;
+}
